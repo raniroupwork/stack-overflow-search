f30de2028b89165aa5cd454e4a76c16d2baeff04
Raniro <errecme@gmail.com>
2021-01-17 09:27:48 -0300

    Remove carbon react, remove menu

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -2,52 +2,33 @@
 import React, { useState } from 'react';
 
 // Styles
-import '@szhsin/react-menu/dist/index.css';
 import './Header.sass';
 
 // Components
 import HistoryMoral from '../../components/HistoryModal/HistoryModal.js'
-import { Menu, MenuItem, ControlledMenu } from '@szhsin/react-menu';
-import Grid from '@material-ui/core/Grid';
+
 
 // Icons
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import { ReactComponent as StackOverflowIcon } from '../../images/Stack_Overflow_icon .svg';
 
 const Header = () => {
     const [isOpen, setOpen] = useState(false);
-    const [auth, setAuthenticaion] = useState(true);
+    const [auth, setAuth] = useState(true);
 
     return (
         <header className="Header">
-            <nav>
-                <Grid
-                direction="row"
-                justify="center"
-                alignItems="center"
-                container>
-                    <Grid 
-                    container
-                    className="Header__logo-icon">
-                        <StackOverflowIcon className="Header__logo-svg"></StackOverflowIcon>
-                        <p className="Header__logo-title">stack <span>overflowGet</span></p>
-                    </Grid>
-                    {auth && (<Grid className="Header__menu-wrapper">
-                        <div className="Header__menu">
-                            <p className="Header__user-name">User Name</p>
-                            <AccountCircleIcon 
-                                className="Header__menu-icon"
-                                onClick={() => setOpen(true)}>
-                            </AccountCircleIcon>
-                            <ControlledMenu className="Header__menu" isOpen={isOpen}
-                                onClose={() => setOpen(false)}>
-                                <MenuItem onClick={(e) => console.log(e)}>Search Historic</MenuItem>
-                                <MenuItem>Logout</MenuItem>
-                            </ControlledMenu>
-                        </div>
-                    </Grid>)}
-                </Grid>
-            </nav>
+            <div className={"Header__logo"}>
+                <StackOverflowIcon className="Header__logo-icon"></StackOverflowIcon>
+                <p className="Header__logo-title">stack <span>overflowGet</span></p>
+            </div>
+            <div className={"Header__menu"}>
+                <div className="Header__menu-user-text">
+                    <p>User Name</p>
+                </div>
+                <div className="Header__menu-content">
+
+                </div>
+            </div>
             {/* <HistoryMoral></HistoryMoral> */}
         </header>
     );
